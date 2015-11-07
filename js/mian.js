63d8998ff6c6594c4c87a57017858d7156d94230
huang2a <huang2a>
2015-11-07 18:51:49 +0800

    update videolist.html page banner
    更新了视频列表的banner位置的滚动jq脚本

--- js/mian.js
+++ js/mian.js
@@ -43,6 +43,17 @@ $(function() {
     });
   });
 $(function() {
+    $('#banner-marquee-box').marquee({
+      auto: false,
+      interval: 5000,
+      speed: 500,
+      showNum: 1,
+      stepLen: 1,
+      prevElement: $('#banner-next'),
+      nextElement: $('#banner-prev')
+    });
+  });
+$(function() {
         $('#slider-marquee').marquee({
             auto: false,
             interval: 5000 ,
